785f0120673936795167bc85702eabc2f88a967a
gabrielrez <97976458+gabrielrez@users.noreply.github.com>
2024-02-18 11:00:54 -0300

    refatorado + localStorage

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ function initNoteApp() {
   const addNoteBtn = document.querySelector(".add-note");
   const events = ["click", "touchstart"];
 
+  window.addEventListener("load", lodadNotesFromLocalStorage);
+
   function openModal(event) {
     event.preventDefault();
     modal.classList.add("active");
@@ -36,6 +38,8 @@ function initNoteApp() {
       const note = createNoteElement(count, titleInput.value, contentInput.value);
       notesArea.append(note);
 
+      saveNoteLocalStorage(count, titleInput.value, contentInput.value);
+
       titleInput.value = "";
       contentInput.value = "";
       modal.classList.remove("active");
@@ -56,10 +60,44 @@ function initNoteApp() {
     return div;
   }
 
+  function saveNoteLocalStorage(id, title, content) {
+    if (typeof Storage !== "undefined") {
+      const existingNotes = JSON.parse(localStorage.getItem("notes")) || [];
+      existingNotes.push({ id, title, content });
+      localStorage.setItem("notes", JSON.stringify(existingNotes));
+    } else {
+      alert("LocalStorage is not supported in this browser");
+    }
+  }
+
+  function removeNoteFromLocalStorage(noteId) {
+    if (typeof Storage !== "undefined") {
+      const existingNotes = JSON.parse(localStorage.getItem("notes")) || [];
+      const updateNotes = existingNotes.filter(note => note.id != noteId);
+      localStorage.setItem("notes", JSON.stringify(updateNotes));
+    } else {
+      alert("LocalStorage is not supported in this browser");
+    }
+  }
+
+  function lodadNotesFromLocalStorage() {
+    if (typeof Storage !== "undefined") {
+      const existingNotes = JSON.parse(localStorage.getItem("notes")) || [];
+
+      existingNotes.forEach((note) => {
+        const noteElement = createNoteElement(note.id, note.title, note.content);
+        notesArea.appendChild(noteElement);
+        addDeleteEvent(noteElement);
+      })
+    }
+  }
+
   function addDeleteEvent(noteElement) {
     const deleteButton = noteElement.querySelector(".delete-btn");
     events.forEach((event) => {
       deleteButton.addEventListener(event, () => {
+        const noteId = noteElement.id;
+        removeNoteFromLocalStorage(noteId);
         noteElement.remove();
       })
     })
